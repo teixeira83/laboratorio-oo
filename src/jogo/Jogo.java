ceb21a047ede54cf894d799ef5ad087a55e37310
davicarvsilva <davicarv13@gmail.com>
2022-06-02 18:50:55 -0300

    Alterações na classe 'Jogo.java'

--- src/jogo/Jogo.java
+++ src/jogo/Jogo.java
@@ -1,7 +1,16 @@
 package jogo;
 
+import testes.TestePalavra;
+import testes.TesteRodada;
+
 public class Jogo {
     public static void main(String[] args) throws Exception {
-        System.out.println("Iniciando Aplicação...");
+    	Aplicacao aplicacao = Aplicacao.getSoleInstance();
+    	
+    	TestePalavra testePalavra = new TestePalavra();
+    	testePalavra.testaPalavra(aplicacao);
+    	
+    	TesteRodada testeRodada = new TesteRodada();
+    	testeRodada.testaRodada(aplicacao);
     }
 }
